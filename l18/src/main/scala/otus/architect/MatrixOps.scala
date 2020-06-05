6958e4dd3a8f025fe911e1e14465bca7e9ce43ec
vzaigrin <vzaigrin@yandex.ru>
2020-06-05 22:17:46 +0300

    Add UML diagram to Lesson 18

--- l18/src/main/scala/otus/architect/MatrixOps.scala
+++ l18/src/main/scala/otus/architect/MatrixOps.scala
@@ -1,7 +1,5 @@
 package otus.architect
 
-import java.io.FileWriter
-
 abstract class MatrixOps {
   val inputFiles: Array[String]
   val outputFile: String
@@ -17,80 +15,4 @@ abstract class MatrixOps {
   protected def doOps(): Unit
 
   protected def output(outputFile: String): Unit
-}
-
-class MatrixMult(override val inputFiles: Array[String], override val outputFile: String) extends MatrixOps {
-  private var matrixA: Matrix = _
-  private var matrixB: Matrix = _
-  private var matrixC: Matrix = _
-
-  override protected def input(inputFiles: Array[String]): Unit = {
-    matrixA = Matrix(inputFiles(0))
-    matrixB = Matrix(inputFiles(1))
-  }
-
-  override protected def doOps(): Unit = {
-    matrixC = matrixA.multiply(matrixB, 1)
-  }
-
-  override protected def output(outputFile: String): Unit = {
-    matrixC.save(outputFile)
-  }
-}
-
-class MatrixAdd(override val inputFiles: Array[String], override val outputFile: String) extends MatrixOps {
-  private var matrixA: Matrix = _
-  private var matrixB: Matrix = _
-  private var matrixC: Matrix = _
-
-  override protected def input(inputFiles: Array[String]): Unit = {
-    matrixA = Matrix(inputFiles(0))
-    matrixB = Matrix(inputFiles(1))
-  }
-
-  override protected def doOps(): Unit = {
-    matrixC = matrixA.add(matrixB, 1)
-  }
-
-  override protected def output(outputFile: String): Unit = {
-    matrixC.save(outputFile)
-  }
-}
-
-class MatrixTrans(override val inputFiles: Array[String], override val outputFile: String) extends MatrixOps {
-  private var matrixA: Matrix = _
-  private var matrixC: Matrix = _
-
-  override protected def input(inputFiles: Array[String]): Unit = {
-    matrixA = Matrix(inputFiles(0))
-  }
-
-  override protected def doOps(): Unit = {
-    matrixC = matrixA.transpose()
-  }
-
-  override protected def output(outputFile: String): Unit = {
-    matrixC.save(outputFile)
-  }
-}
-
-class MatrixDet(override val inputFiles: Array[String], override val outputFile: String) extends MatrixOps {
-  private var matrixA: Matrix = _
-  private var dt: Int = _
-
-  override protected def input(inputFiles: Array[String]): Unit = {
-    matrixA = Matrix(inputFiles(0))
-  }
-
-  override protected def doOps(): Unit = {
-    dt = matrixA.det()
-  }
-
-  override protected def output(outputFile: String): Unit = {
-    val fw = new FileWriter(outputFile, false)
-    try {
-      fw.write(s"$dt")
-    }
-    finally fw.close()
-  }
 }
